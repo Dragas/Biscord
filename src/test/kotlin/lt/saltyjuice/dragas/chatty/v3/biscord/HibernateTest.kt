85733bd039f0a62551c334767a323f12759bc99d
Mantas Gridinas <mgridinas@gmail.com>
2017-11-16 12:41:42 +0200

    removes ignore notice

--- src/test/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/HibernateTest.kt
+++ src/test/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/HibernateTest.kt
@@ -3,7 +3,6 @@ package lt.saltyjuice.dragas.chatty.v3.biscord
 import lt.saltyjuice.dragas.chatty.v3.biscord.entity.Card
 import lt.saltyjuice.dragas.chatty.v3.biscord.utility.HibernateUtil
 import org.junit.Assert
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -12,7 +11,6 @@ import org.junit.runners.JUnit4
 class HibernateTest
 {
     @Test
-    @Ignore("Should only be run on local instances")
     fun hibernateInitializes()
     {
         HibernateUtil.executeTransaction({ session ->
