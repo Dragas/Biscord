d6163dc5218068ab4732c76cc53d2b9974f53348
Dragas <mgridinas@gmail.com>
2017-11-16 22:48:00 +0200

    adds artwork

--- src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/entity/Card.kt
+++ src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/entity/Card.kt
@@ -80,6 +80,11 @@ open class Card : Comparable<Card>
         {
             return "https://art.hearthstonejson.com/v1/render/latest/enUS/256x/$cardId.png"
         }
+    val artwork: String
+        get()
+        {
+            return "https://art.hearthstonejson.com/v1/512x/$cardId.jpg"
+        }
     @Expose
     @SerializedName("imgGold")
     var imgGold: String = ""
