c8a5a5dc30d5505bf8ef83593bc2082b197e20b0
Mantas Gridinas <mgridinas@gmail.com>
2017-11-16 15:24:33 +0200

    migrates to persistant thread saving

--- src/test/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/HibernateTest.kt
+++ src/test/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/HibernateTest.kt
@@ -1,7 +1,10 @@
 package lt.saltyjuice.dragas.chatty.v3.biscord
 
+import lt.saltyjuice.dragas.chatty.v3.biscord.controller.StalkingController
 import lt.saltyjuice.dragas.chatty.v3.biscord.entity.Card
+import lt.saltyjuice.dragas.chatty.v3.biscord.entity.KThread
 import lt.saltyjuice.dragas.chatty.v3.biscord.utility.HibernateUtil
+import lt.saltyjuice.dragas.utility.khan4.entity.Thread
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,6 +20,19 @@ class HibernateTest
             val query = session.createQuery("from Card", Card::class.java)
             val result = query.resultList
             Assert.assertTrue(result.isEmpty())
+        })
+    }
+
+    @Test
+    fun savesThreadsCorrectly()
+    {
+        val thread = Thread()
+        thread.postNumber = 1
+        StalkingController.saveThread(thread)
+        HibernateUtil.executeSimpleTransaction({ session ->
+            val query = session.createQuery("from KThread", KThread::class.java)
+            val result = query.resultList
+            Assert.assertTrue(result.isNotEmpty())
         })
     }
 }
