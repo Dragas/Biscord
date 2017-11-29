0cf7efc552160e28ec6af62d82f58656ca9b0740
Mantas Gridinas <mgridinas@gmail.com>
2017-11-29 15:58:49 +0200

    adds user ids

--- src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/controller/CardController.kt
+++ src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/controller/CardController.kt
@@ -23,7 +23,7 @@ class CardController : Controller
                 .map(MatchResult::value)
                 .map { it.replace("[[", "").replace("]]", "").trim() }
                 .filter(String::isNotBlank)
-                .map { "hscard $it -chid ${request.channelId} -s -co" }
+                .map { "hscard $it -chid ${request.channelId} -s -co -user ${request.author.id}" }
                 .forEach(KommanderController.Companion::execute)
     }
 
